55bd06c121d39ae0b78987c8170b7d856184a9e6
LexTheMenace <lexthemenace@gmail.com>
2020-10-17 14:40:38 -0400

    frontend, header footer; products & ratings; router & single product screen;

---
+++ client/src/products.js
@@ -0,0 +1,69 @@
+const products = [
+    {
+      _id: '1',
+      name: 'Computer Glasses',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61iVUN7Hx2L._AC_UL1500_.jpg',
+      description:
+        'Block computer light that gives you a headache.',
+      brand: 'Gamma Ray Optics',
+      category: 'Office Supplies',
+      price: 19.99,
+      countInStock: 15,
+      rating: 4.8,
+      numReviews: 7,
+    },
+    {
+      _id: '2',
+      name: 'Wireless Keyboard and Mouse',
+      image: 'https://store.hp.com/app/assets/images/product/LV290AA%23ABA/center_facing.png?_=1552333142043&imwidth=270&imdensity=1',
+      description:
+        'Get that 2001 desktop feel back.',
+      brand: 'HP',
+      category: 'Electronics',
+      price: 29.99,
+      countInStock: 100,
+      rating: 3.6,
+      numReviews: 453,
+    },
+    {
+      _id: '3',
+      name: 'Kenneth Cole Dress Shirt',
+      image: 'https://image.menswearhouse.com/is/image/TMW/MW40_55W2_01_AWEARNESS_KENNETH_COLE_WHITE_SET?$40MainPDP$',
+      description:
+        'Perfect for those dinners out, not so good for messy eaters.',
+      brand: 'Kenneth Cole',
+      category: 'Clothing',
+      price: 49.99,
+      countInStock: 7,
+      rating: 4.5,
+      numReviews: 52,
+    },
+    {
+      _id: '4',
+      name: 'HP OMEN Laptop',
+      image: 'https://store.hp.com/app/assets/images/product/17H47AV_1/center_facing.png?_=1591174341996&amp;imwidth=430&amp;imdensity=1',
+      description:
+        'Stunning desktop-grade power in a sleek, portable design keeps you playing your best wherever gaming takes you. Take on the most demanding AAA game titles with NVIDIA® graphics and an Intel® Core™ processor. Multi-core is designed to improve performance of certain software products. Not all customers or software applications will necessarily benefit from use of this technology. Performance and clock frequency will vary depending on application workload and your hardware and software configurations. Intel’s numbering, branding and/or naming is not a measurement of higher performance. Immerse yourself in smooth gameplay on a narrow-bezel display with fast refresh rates.',
+      brand: 'HP',
+      category: 'Electronics',
+      price: 1399.99,
+      countInStock: 3,
+      rating: 4.7,
+      numReviews: 32,
+    },
+    {
+        _id: '5',
+        name: 'Chair',
+        image: 'https://www.staples-3p.com/s7/is/image/Staples/sp41816818_sc7',
+        description:
+          'If you have to sit for 8 hours, it might as well be in this chair!',
+        brand: 'Staples',
+        category: 'Office Supplies',
+        price: 152.99,
+        countInStock: 20,
+        rating: 4.5,
+        numReviews: 796,
+      },
+  ]
+  
+  export default products;
